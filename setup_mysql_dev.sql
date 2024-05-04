74e7668cb763d2ba7132d2db791228582d2acaf1
asenjialvin <asenjialvin@gmail.com>
2024-05-04 22:53:36 +0300

    Added all tasks

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,17 +1,7 @@
--- Create a development database
-CREATE DATABASE IF NOT EXISTS my_dev_database;
+-- prepares a MySQL server for the project
 
--- Use the development database
-USE my_dev_database;
-
--- Create a table
-CREATE TABLE IF NOT EXISTS my_table (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50)
-);
-
--- Create a user for development
-CREATE USER 'alvin'@'localhost' IDENTIFIED BY 'root';
-
--- Grant privileges to the user
-GRANT ALL PRIVILEGES ON my_dev_database.* TO 'alvin'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
